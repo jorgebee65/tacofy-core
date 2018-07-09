d7f63852bcf32c1c9dae2aa76079d3661f9769ec
Jorge Dominguez <jorgebee65@hotmail.com>
2018-07-09 00:20:34 -0500

    fix servicio Taqueria, probado desde postman, primera prueba exitosa

--- src/main/java/com/tacofy/service/impl/TaqueriaServiceImpl.java
+++ src/main/java/com/tacofy/service/impl/TaqueriaServiceImpl.java
@@ -3,11 +3,13 @@ package com.tacofy.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.tacofy.dao.TaqueriaDAO;
 import com.tacofy.po.Taqueria;
 import com.tacofy.service.TaqueriaService;
 
+@Service
 public class TaqueriaServiceImpl implements TaqueriaService {
 	
 	@Autowired
